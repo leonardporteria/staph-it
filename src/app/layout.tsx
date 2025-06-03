e91859bfe451a2ad85ed7228d451cb1631ad67a9
leonardporteria <porterialeonard@gmail.com>
2025-06-03 19:03:59 +0800

    fonts adjustemtn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased relative max-w-screen w-full`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative max-w-screen w-full font-smoothcirculars`}
         suppressHydrationWarning
       >
         <ThemeProvider
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        smoothcirculars: ['Gainsborough', 'serif'],
+      },
+    },
+  },
+};
